4ae00685a4c8064b2b7337a2f5c1ef9703b42011
Ahmed Elashry <ahmedelashry70@gmail.com>
2023-11-08 10:48:13 +0100

    take the db id as input and use numpy nan instead of None

--- src/notion.py
+++ src/notion.py
@@ -7,31 +7,33 @@ done on notion and consequently make the changes in memrise.
 import os
 import requests
 import pandas as pd
+import numpy as np
 # ? the right way to import from constants ?
 from .constants import COL_LIST
 
 NOTION_SECRET = os.environ.get("NOTION_SECRET")
 NOTION_VERSION = "2022-06-28"  # Notion API version from their website
-# ? can I get the ID using Notion integration?
-NOTION_DATABASE_ID = "7c83b2ef86e941b986e8c8461fb5134d"
 
 HEADERS = {
     "Authorization": f"Bearer {NOTION_SECRET}",
     "Notion-Version": "2022-06-28",
     "Content-Type": "application/json",
 }
-ENDPOINT = f"https://api.notion.com/v1/databases/{NOTION_DATABASE_ID}/query"
+# a list in case I add more date columns
+DATE_COL_LIST = ["date modified"]  # only used here and in testing
 
-DATE_COL_LIST = ["date created", "date modified"]  # only used here
-
-def get_data_from_notion():
+def get_data_from_notion(notion_db_id: str):
     """Gets and processes data from Notion via API.
+
+    Args:
+        notion_db_id (str): ID of the Notion database.
 
     Returns:
         pandas.DataFrame: dataframe of the entries in the Notion database.
     """
+    endpoint = f"https://api.notion.com/v1/databases/{notion_db_id}/query"
     response = requests.post(
-        ENDPOINT,
+        endpoint,
         headers=HEADERS,
         timeout=10,
     )
@@ -40,7 +42,7 @@ def get_data_from_notion():
     while has_more:
         next_cursor = response.json()["next_cursor"]
         response = requests.post(
-            ENDPOINT,
+            endpoint,
             headers=HEADERS,
             timeout=10,
             json={
@@ -78,6 +80,8 @@ def handle_response(response):
                                                  axis=1)
     new_df = pd.DataFrame(df_copy["properties"].tolist())
     for col in new_df:
+        if np.all(new_df[col].isna()):  # new_df[col].str will throw an error
+            continue
         new_df[col] = new_df[col].str.strip()
     for col in DATE_COL_LIST:
         new_df[col] = pd.to_datetime(new_df[col])
@@ -137,16 +141,19 @@ def get_property_content(property_dict):
         if len(rich_text_list) > 0:
             return rich_text_list[0]["plain_text"]
         else:
-            return None
+            return np.nan
     elif property_type == "select":
         select_dict = property_dict.get("select", {})
         # if None selected, select_dict will be None
         select_dict = {} if select_dict is None else select_dict
-        return select_dict.get("name", None)
+        return select_dict.get("name", np.nan)
     elif property_type == "multi_select":
         select_list = [
             select["name"] for select in property_dict["multi_select"]
         ]
-        return ", ".join(select_list)
+        if select_list:
+            return ", ".join(select_list)
+        else:
+            return np.nan
     else:
-        return property_dict.get(property_type, None)
+        return property_dict.get(property_type, np.nan)
